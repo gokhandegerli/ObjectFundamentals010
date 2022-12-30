3176016adbe27d5993dba7ced6dd136e4859609a
gokhandegerli <gokhan.degerli.sd@gmail.com>
2022-12-30 16:28:07 +0300

    new chapters

--- src/com/godoro/OF13/computer/Computer.java
+++ src/com/godoro/OF13/computer/Computer.java
@@ -1,16 +1,55 @@
 package com.godoro.OF13.computer;
 
+import com.godoro.OF13.printer.LaserPrinter;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Computer {
 
-    private Usb usb; //Tek Usb'si olduğunu varsaydık.
-
-
-
+/*    private Usb usb; //Tek Usb'si olduğunu varsaydık.
     public Usb getUsb() {
         return usb;
     }
-
     public void setUsb(Usb usb) {
         this.usb = usb;
+    }*/
+
+    private List<Usb> usbList = new ArrayList<>();
+
+    public List<Usb> getUsbList() {
+        return usbList;
     }
+    public void setUsbList(List<Usb> usbList) {
+        this.usbList = usbList;
+    }
+
+    public void testUsbList() {
+        System.out.println("\r\nTESTING USB LIST....");
+        for (Usb usb : usbList){
+            testUsb(usb);
+        }
+    }
+
+    public void testUsb(Usb usb) {
+        System.out.println("\r\nTESTING....");
+        usb.connect();
+        String data = usb.read();
+        System.out.println("USB'den okunan veri" + data);
+        usb.write("Bir veri");
+        usb.disconnect();
+    }
+
+/*   public void testUsb() {
+        System.out.println("\r\nTESTING....");
+        usb.connect();
+        String data = usb.read();
+        System.out.println("USB'den okunan veri" + data);
+        usb.write("Bir veri");
+        usb.disconnect();
+    }*/
+
+
+
+
 }
--- src/com/godoro/OF13/test/Test.java
+++ src/com/godoro/OF13/test/Test.java
@@ -1,8 +1,37 @@
 package com.godoro.OF13.test;
 
+import com.godoro.OF13.computer.Computer;
+import com.godoro.OF13.computer.Usb;
+import com.godoro.OF13.mouse.AdvancedMouse;
+import com.godoro.OF13.printer.LaserPrinter;
+
 public class Test {
 
     public static void main(String[] args) {
+
+/*      Tek fare ya da printer için yapılan örnek
+        Computer computer = new Computer();
+        AdvancedMouse mouse = new AdvancedMouse();
+        computer.setUsb(mouse);
+        computer.testUsb();
+
+        LaserPrinter printer = new LaserPrinter();
+        computer.setUsb(printer);
+        computer.testUsb();*/
+
+        //Liste ile olan örnek için
+        Computer computer = new Computer();
+        Usb mouse = new AdvancedMouse(); //AdvancedMouse mouse = new AdvancedMouse(); desek de olurdu.
+        computer.getUsbList().add(mouse);
+        computer.testUsb(mouse); // tek olarak fareyi test ediyor.
+
+        LaserPrinter printer = new LaserPrinter();
+        computer.getUsbList().add(printer);
+        computer.testUsb(printer); // tek olarak printer test ediyor
+
+
+        computer.testUsbList(); // tüm Usblerin test metodunu çağırıyor sırayla.
+
 
 
 
