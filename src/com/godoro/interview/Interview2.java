6e847d0c469e923cd603d975a9d6d9ef60d31808
gokhandegerli <gokhan.degerli.sd@gmail.com>
2023-01-10 00:19:50 +0300

    new interview queries added

--- src/com/godoro/interview/Interview2.java
+++ src/com/godoro/interview/Interview2.java
@@ -1,6 +1,11 @@
 package com.godoro.interview;
 
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Interview2 {
 
@@ -70,14 +75,14 @@ public class Interview2 {
         return response;
     }
 
-    public static void vowelsConsonants ( String text) {
+    public static void vowelsConsonants(String text) {
 
-        int vowel=0;
-        int consonant=0;
+        int vowel = 0;
+        int consonant = 0;
 
-        for (int i=0;i<text.length();i++) {
+        for (int i = 0; i < text.length(); i++) {
             char c = text.charAt(i);
-            if (c=='a' || c =='e' || c == 'ı' || c == 'i' || c == 'o' || c == 'ö' || c == 'u' || c == 'ü') {
+            if (c == 'a' || c == 'e' || c == 'ı' || c == 'i' || c == 'o' || c == 'ö' || c == 'u' || c == 'ü') {
                 vowel++;
             } else {
                 consonant++;
@@ -85,6 +90,63 @@ public class Interview2 {
         }
         System.out.println("Vowels: " + vowel);
         System.out.println("Consonants: " + consonant);
+
+    }
+
+    public static String swap(StringBuilder s) {
+
+        char first = s.charAt(0);
+        int lastIndex = s.length() - 1;
+        char last = s.charAt(s.length() - 1);
+
+        s = s.deleteCharAt(lastIndex);
+        s = s.deleteCharAt(0);
+
+        String newText;
+
+        newText = last + s.toString() + first;
+
+        return newText;
+
+    }
+
+    public static void checkContainIndexes(List<String> list, String text) {
+        List<Integer> indexes = new ArrayList<>();
+        //list.stream().filter(v -> v==text).map(v -> list.indexOf(v)).toList();
+
+        for (int i = 0; i < list.size(); i++) {
+
+            if (list.get(i) == text) {
+                indexes.add(i);
+            }
+        }
+
+        List<String> matchedValues = list.stream().filter(l -> l.contains(text)).toList();
+
+        indexes.forEach(System.out::println); // prints [0, 4]
+        matchedValues.forEach(System.out::println);
+    }
+
+    public static void fibonacciNumbers(int fibonacciIter) {
+
+        int f0 = 1;
+        int f1 = 1;
+        int fibonacciNumber;
+
+        System.out.format("%8d", f0);
+        System.out.format("%8d", f1);
+
+        for (int i = 3; i <= fibonacciIter; i++) {
+            fibonacciNumber = f0 + f1;
+            System.out.format("%8d", fibonacciNumber);
+
+            f0 = f1;
+            f1 = fibonacciNumber;
+
+            if (i % 10 == 0) {
+                System.out.println();
+            }
+        }
 
     }
 
--- src/com/godoro/interview/InterviewTest.java
+++ src/com/godoro/interview/InterviewTest.java
@@ -1,5 +1,9 @@
 package com.godoro.interview;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class InterviewTest {
 
     public static void main(String[] args) {
@@ -28,7 +32,7 @@ public class InterviewTest {
         Interview.vowelsConsonants("ankara");
         System.out.println("--------------------");*/
 
-        boolean isPalindrome = Interview2.isPalindrome("Gökhan");
+ /*       boolean isPalindrome = Interview2.isPalindrome("Gökhan");
         System.out.println(isPalindrome);
         System.out.println("--------------------");
 
@@ -50,6 +54,18 @@ public class InterviewTest {
 
         Interview2.vowelsConsonants("istanbul");
         System.out.println("--------------------");
+
+        System.out.println(Interview2.swap(new StringBuilder("Samuele Dalla Bona")));
+        System.out.println("--------------------");
+
+
+        List<String> values = Arrays.asList("asa", "def", "ghi", "a", "ab", "abc", "abc");
+        Interview2.checkContainIndexes(values,"abc");
+        System.out.println("--------------------");*/
+
+        Interview2.fibonacciNumbers(30);
+        System.out.println("--------------------");
+
 
     }
 }
